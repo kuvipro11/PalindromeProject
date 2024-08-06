51c54adac1962e9ab914f0f4fbc3b88fb5c0e644
Kuvindu <kuvinduda11@gmail.com>
2024-08-06 17:43:46 +0530

    JS - Modify String function

---
+++ script.js
@@ -0,0 +1,11 @@
+const userInput = document.getElementById("user-input");
+const submitBtn = document.getElementById("submit-btn");
+const result = document.getElementById("result");
+
+let resultIsShowing = false;
+
+function modifyString(str) {
+    let modifiedString = str.toLowerCase();
+    return modifiedString.replace(/[^\d\w\s]/g, "");    
+}
+
